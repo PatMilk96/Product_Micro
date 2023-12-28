f0839fd6eb9db095e0be451a83ccb73536d30c7e
Patryk Milkiewicz <g00397265@atu.ie>
2023-12-28 19:03:57 +0000

    Added Controller Code with an endpoint to pass a product to the database microservice that will be added to the database

--- src/main/java/ie/atu/product_micro/ProductController.java
+++ src/main/java/ie/atu/product_micro/ProductController.java
@@ -1,4 +1,20 @@
 package ie.atu.product_micro;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    @Autowired
+    private RepositoryService repositoryService;
+
+    @PostMapping("/addProduct")
+    public String addProduct(@RequestBody Product product){
+        String response = repositoryService.addProduct(product);
+        return response;
+    }
 }
